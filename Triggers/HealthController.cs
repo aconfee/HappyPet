63b6f22a6a7c7fddd67d70a5be227b5461f88693
Adam <adamestela@gmail.com>
2020-07-01 21:38:34 -0500

    Added style cop and ARM templates.

--- Triggers/HealthController.cs
+++ Triggers/HealthController.cs
@@ -1,11 +1,9 @@
-using System.IO;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
-using Newtonsoft.Json;
 
 namespace HappyPet
 {
@@ -18,17 +16,9 @@ namespace HappyPet
             [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)] HttpRequest request,
             ILogger log)
         {
-            log.LogInformation("C# HTTP trigger function processed a request.");
+            log.LogInformation("Health endpoint triggered.");
 
-            string name = request.Query["name"];
-
-            string requestBody = await new StreamReader(request.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
-
-            return name != null
-                ? (ActionResult)new OkObjectResult($"Hello, {name}")
-                : new BadRequestObjectResult("Please pass a name on the query string or in the request body");
+            return new OkObjectResult("API running.");
         }
     }
 }
